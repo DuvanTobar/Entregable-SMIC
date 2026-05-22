fa9f46fab6510a6637d4ec64a844c84c92c07437
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2026-05-22 18:52:02 +0000

    Commit Final.

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ Para este proyecto se usaran sensores de proximidad, bombas de agua, sensores de
 Cada uno de los datos y estados de las partes de la casa se veran reflejados en Node-Red, en donde se podran activar ciertas acciones para automatizar a distancia el hogar.
 ![alt text](image.png)
 Los avances actuales se han fundamentado en la union de varios codigos para generar el primer codigo principal que automatice la casa inteligente. Se hizo uso de librerias especiales para los sensores RFID o el rele de la bomba de agua junto al sensor de humedad. Asi mismo tambien se implementaron funciones nuevas como las del movimiento del servomotor o las de la deteccion de cada elemento en el codigo con una respuesta a la pagina que se encuentra en NODE-RED. En el siguiente diagrama se encuentra el estado actual de las conexiones y como van conectadas a cada microcontrolador que generen sus respuestas al servidor.
-![alt text](<WhatsApp Image 2026-04-28 at 11.06.50 PM.jpeg>)
-Asi mismo en el apartado de main.ino se encuentra tanto el codigo que contiene a los lectores con la bomba de agua y el sensor de humedad, junto a otro codigo el cual contiene todo el funcionamiento de los servomotores para la apertura del techo y el riego de las plantas en la huerta.
+![alt text](<Diagrama Proyecto..jpeg>)
+Aca se encuentra el diagrama final del proyecto, junto a su codigo final, donde todos los avances se implementaron en su diseño final con el chasis.
--- src/main.ino
+++ src/main.ino
@@ -1,66 +1,88 @@
+//Codigo que combina la logica de ambas ESP, tanto la ESP32 como la ESP32-S3, este seria el completo.
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <DHT.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <DHT.h>
+#include <ESP32Servo.h>
 
-const char* ssid        = "A36 de Duvan";
-const char* password    = "SarisHermosa";
-const char* mqtt_server = "broker.hivemq.com";
-const int   mqtt_port   = 1883;
+const char* SSID      = "Redmi";
+const char* PASS      = "23092018";
+const char* MQTT_HOST = "broker.hivemq.com";
+const int   MQTT_PORT = 1883;
 
+#define TOPIC_RFID1        "esp32/rfid1"
+#define TOPIC_RFID2        "esp32/rfid2"
+#define TOPIC_CMD_MOTOR    "esp32/cmd/motor"
+#define TOPIC_STATUS       "esp32/status"
 #define TOPIC_TEMP         "invernadero/temperatura"
 #define TOPIC_HUM          "esp32/hum"
 #define TOPIC_BOMBA        "esp32/bomb"
 #define TOPIC_TECHO        "invernadero/techo/estado"
-#define TOPIC_RFID1        "esp32/rfid"
-#define TOPIC_RFID2        "esp32/rfid2"
 #define TOPIC_ACCESO1      "invernadero/acceso/puerta1"
 #define TOPIC_ACCESO2      "invernadero/acceso/puerta2"
 #define TOPIC_CMD_BOMBA    "invernadero/cmd/bomba"
+#define TOPIC_MOVIMIENTO1  "esp32/movimiento1"
+#define TOPIC_SERVO1       "esp32/servo1"
+#define TOPIC_MOTORES      "esp32/motores"
+
+#define SCK_PIN   12
+#define MISO_PIN  13
+#define MOSI_PIN  11
+#define SS_1_PIN  10
+#define RST_1_PIN  9
+#define SS_2_PIN  15
+#define RST_2_PIN 21
+
+#define L293D_ENA  38
+#define L293D_IN1  39
+#define L293D_IN2  40
+#define VELOCIDAD  200
 
 #define DHTPIN   25
 #define DHTTYPE  DHT11
+
+#define PIN_RELE    23
+#define SERVO1_PIN   4
+#define PIR1_PIN    14
+
+#define PWM_FREQ_HZ    5000
+#define PWM_RESOLUTION    8
+#define DEBOUNCE_MS    2000
+#define HEARTBEAT_MS  30000
+#define WIFI_RETRY_MS  5000
+#define MQTT_RETRY_MS  3000
+
+SPIClass spiRFID(FSPI);
+MFRC522 rfid1(SS_1_PIN, RST_1_PIN);
+MFRC522 rfid2(SS_2_PIN, RST_2_PIN);
+
 DHT dht(DHTPIN, DHTTYPE);
+Servo servo1;
 
-#define PIN_RELE 23
+WiFiClient   wifiClient;
+PubSubClient mqtt(wifiClient);
 
-#define SPI_SCK   12
-#define SPI_MISO  13
-#define SPI_MOSI  11
-#define SS_PIN1   10
-#define RST_PIN1  9
-#define SS_PIN2   14 
-#define RST_PIN2  21 
+String lastUID1 = "", lastUID2 = "";
+unsigned long lastTime1 = 0, lastTime2 = 0;
+unsigned long lastHeartbeat = 0, lastWifiCheck = 0, lastMqttRetry = 0;
+unsigned long ultimaLectura = 0, ultimoEncendido = 0;
 
-MFRC522 mfrc522_1(SS_PIN1, RST_PIN1);
-MFRC522 mfrc522_2(SS_PIN2, RST_PIN2);
+int  motorSpeed = 0;
+bool motorDir   = true;
+bool techoCerrado     = false;
+bool motoresActivos   = false;
+bool motoresEncendidos = false;
+bool servoActivo      = false;
+unsigned long servoInicio = 0;
+int pirAnterior = LOW;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+const unsigned long INTERVALO_MOTOR = 10000;
+const unsigned long DURACION_MOTOR  =  3000;
+const unsigned long SERVO_MS        =  1000;
+const unsigned long INTERVALO_SENSOR = 3000;
 
-unsigned long ultimaLectura = 0;
-const long intervaloSensor = 3000;
-
-bool techoCerrado = false;
-
-void activarBomba(int segundos) {
-  Serial.println("Regando plantas...");
-  client.publish(TOPIC_BOMBA, "encendida");
-
-  digitalWrite(PIN_RELE, LOW);
-  delay(segundos * 1000);
-
-  digitalWrite(PIN_RELE, HIGH);
-  client.publish(TOPIC_BOMBA, "apagada");
-}
-
-void apagarBomba() {
-  digitalWrite(PIN_RELE, HIGH);
-  client.publish(TOPIC_BOMBA, "apagada");
-}
-
-String getUID(MFRC522 &lector) {
+String uidToString(MFRC522& lector) {
   String uid = "";
   for (byte i = 0; i < lector.uid.size; i++) {
     if (lector.uid.uidByte[i] < 0x10) uid += "0";
@@ -71,206 +93,228 @@ String getUID(MFRC522 &lector) {
   return uid;
 }
 
-void detectarTarjeta(MFRC522 &lector, int num, String uid) {
-  const char* topicRFID   = (num == 1) ? TOPIC_RFID1   : TOPIC_RFID2;
-  const char* topicAcceso = (num == 1) ? TOPIC_ACCESO1 : TOPIC_ACCESO2;
-
-  Serial.print("[Lector "); Serial.print(num);
-  Serial.print("] UID: "); Serial.println(uid);
-
-  client.publish(topicRFID, uid.c_str());
-  client.publish(topicAcceso, "detectado");
-
-  delay(1500);
+void motorStop() {
+  ledcWrite(L293D_ENA, 0);
+  digitalWrite(L293D_IN1, LOW);
+  digitalWrite(L293D_IN2, LOW);
+  motorSpeed = 0;
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  String mensaje = "";
-  for (unsigned int i = 0; i < length; i++) {
-    mensaje += (char)payload[i];
-  }
-
-  mensaje.trim();
-
-  if (String(topic) == TOPIC_CMD_BOMBA) {
-    if (mensaje == "ON") activarBomba(3);
-    if (mensaje == "OFF") apagarBomba();
-  }
+void motorSet(int speed, bool forward) {
+  speed = constrain(speed, 0, 255);
+  motorSpeed = speed;
+  motorDir   = forward;
+  if (speed == 0) { motorStop(); return; }
+  digitalWrite(L293D_IN1, forward ? HIGH : LOW);
+  digitalWrite(L293D_IN2, forward ? LOW  : HIGH);
+  ledcWrite(L293D_ENA, speed);
 }
 
-void setup_wifi() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+void parseMotorCommand(const String& payload) {
+  int speedVal = motorSpeed;
+  int idx = payload.indexOf("\"speed\":");
+  if (idx != -1) speedVal = payload.substring(idx + 8).toInt();
+  bool fwd = motorDir;
+  if (payload.indexOf("fwd") != -1) fwd = true;
+  if (payload.indexOf("rev") != -1) fwd = false;
+  motorSet(speedVal, fwd);
 }
 
-void reconnect() {
-  while (!client.connected()) {
-    if (client.connect("ESP32-Invernadero")) {
-      client.subscribe(TOPIC_CMD_BOMBA);
+void activarBomba(int segundos) {
+  mqtt.publish(TOPIC_BOMBA, "encendida");
+  digitalWrite(PIN_RELE, LOW);
+  delay(segundos * 1000);
+  digitalWrite(PIN_RELE, HIGH);
+  mqtt.publish(TOPIC_BOMBA, "apagada");
+}
+
+void apagarBomba() {
+  digitalWrite(PIN_RELE, HIGH);
+  mqtt.publish(TOPIC_BOMBA, "apagada");
+}
+
+void abrirServo() {
+  servo1.write(180);
+  servoActivo = true;
+  servoInicio = millis();
+}
+
+void wifiConnect() {
+  if (WiFi.status() == WL_CONNECTED) return;
+  WiFi.begin(SSID, PASS);
+  unsigned long t = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - t < 10000) delay(300);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String msg = "", t = String(topic);
+  for (unsigned int i = 0; i < length; i++) msg += (char)payload[i];
+  msg.trim();
+
+  if (t == TOPIC_CMD_MOTOR)  parseMotorCommand(msg);
+  if (t == TOPIC_CMD_BOMBA)  { if (msg == "ON") activarBomba(3); else if (msg == "OFF") apagarBomba(); }
+  if (t == TOPIC_SERVO1)     { if (msg == "1" || msg == "true") abrirServo(); }
+  if (t == TOPIC_MOTORES) {
+    if (msg == "1" || msg == "true") {
+      motoresActivos  = true;
+      ultimoEncendido = millis() - INTERVALO_MOTOR;
     } else {
-      delay(2000);
+      motoresActivos    = false;
+      motoresEncendidos = false;
+      digitalWrite(L293D_IN1, LOW);
+      digitalWrite(L293D_IN2, LOW);
+      analogWrite(L293D_ENA, 0);
     }
   }
 }
 
+bool mqttConnect() {
+  if (mqtt.connected()) return true;
+  if (WiFi.status() != WL_CONNECTED) return false;
+  String clientId = "ESP32S3-" + String((uint32_t)ESP.getEfuseMac(), HEX);
+  bool ok = mqtt.connect(clientId.c_str(), nullptr, nullptr,
+                         TOPIC_STATUS, 0, false, "{\"online\":false}");
+  if (ok) {
+    mqtt.subscribe(TOPIC_CMD_MOTOR);
+    mqtt.subscribe(TOPIC_CMD_BOMBA);
+    mqtt.subscribe(TOPIC_SERVO1);
+    mqtt.subscribe(TOPIC_MOTORES);
+    mqtt.publish(TOPIC_STATUS, "{\"online\":true}", false);
+  }
+  return ok;
+}
+
 void setup() {
   Serial.begin(115200);
+  delay(500);
+
+  pinMode(SS_1_PIN, OUTPUT); digitalWrite(SS_1_PIN, HIGH);
+  pinMode(SS_2_PIN, OUTPUT); digitalWrite(SS_2_PIN, HIGH);
+  spiRFID.begin(SCK_PIN, MISO_PIN, MOSI_PIN);
+  rfid1.PCD_Init(&spiRFID);
+  rfid1.PCD_SetAntennaGain(rfid1.RxGain_max);
+  rfid2.PCD_Init(&spiRFID);
+  rfid2.PCD_SetAntennaGain(rfid2.RxGain_max);
+
+  ledcAttach(L293D_ENA, PWM_FREQ_HZ, PWM_RESOLUTION);
+  pinMode(L293D_IN1, OUTPUT); pinMode(L293D_IN2, OUTPUT);
+  motorStop();
 
   pinMode(PIN_RELE, OUTPUT);
   digitalWrite(PIN_RELE, HIGH);
 
   dht.begin();
 
-  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-  mfrc522_1.PCD_Init();
-  mfrc522_2.PCD_Init();
+  servo1.setPeriodHertz(50);
+  servo1.attach(SERVO1_PIN, 500, 2400);
+  servo1.write(0);
 
-  setup_wifi();
+  pinMode(PIR1_PIN, INPUT);
 
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
+  wifiConnect();
+
+  mqtt.setServer(MQTT_HOST, MQTT_PORT);
+  mqtt.setCallback(mqttCallback);
+  mqtt.setKeepAlive(60);
+  mqtt.setBufferSize(512);
+  mqttConnect();
 }
 
 void loop() {
-  if (!client.connected()) reconnect();
-  client.loop();
+  unsigned long now = millis();
 
-  unsigned long ahora = millis();
+  if (now - lastWifiCheck > WIFI_RETRY_MS) {
+    lastWifiCheck = now;
+    if (WiFi.status() != WL_CONNECTED) wifiConnect();
+  }
 
-  if (ahora - ultimaLectura >= intervaloSensor) {
-    ultimaLectura = ahora;
+  if (!mqtt.connected() && now - lastMqttRetry > MQTT_RETRY_MS) {
+    lastMqttRetry = now;
+    mqttConnect();
+  }
+  mqtt.loop();
 
+  if (now - lastHeartbeat > HEARTBEAT_MS) {
+    lastHeartbeat = now;
+    String hb = "{\"online\":true,\"rssi\":" + String(WiFi.RSSI()) +
+                ",\"motor\":" + String(motorSpeed) + "}";
+    mqtt.publish(TOPIC_STATUS, hb.c_str(), false);
+  }
+
+  if (now - ultimaLectura >= INTERVALO_SENSOR) {
+    ultimaLectura = now;
     float hum  = dht.readHumidity();
     float temp = dht.readTemperature();
-
     if (!isnan(hum) && !isnan(temp)) {
-
       char bufTemp[8], bufHum[8];
       dtostrf(temp, 4, 1, bufTemp);
-      dtostrf(hum, 4, 1, bufHum);
-
-      client.publish(TOPIC_TEMP, bufTemp);
-      client.publish(TOPIC_HUM, bufHum);
-
-      Serial.print("Temp: "); Serial.print(temp);
-      Serial.print(" Hum: "); Serial.println(hum);
-
-      if (temp > 24 && !techoCerrado) {
-        client.publish(TOPIC_TECHO, "cerrado");
+      dtostrf(hum,  4, 1, bufHum);
+      mqtt.publish(TOPIC_TEMP, bufTemp);
+      mqtt.publish(TOPIC_HUM,  bufHum);
+      if (temp > 26 && !techoCerrado) {
+        mqtt.publish(TOPIC_TECHO, "cerrado");
         activarBomba(3);
         techoCerrado = true;
-
-      } else if (temp <= 24 && techoCerrado) {
-        client.publish(TOPIC_TECHO, "abierto");
+      } else if (temp <= 24 && hum < 70 && techoCerrado) {
+        mqtt.publish(TOPIC_TECHO, "abierto");
         apagarBomba();
         techoCerrado = false;
       }
+      if (temp > 26 && hum < 70) activarBomba(3);
     }
   }
 
-  if (mfrc522_1.PICC_IsNewCardPresent() && mfrc522_1.PICC_ReadCardSerial()) {
-    detectarTarjeta(mfrc522_1, 1, getUID(mfrc522_1));
-    mfrc522_1.PICC_HaltA();
-    mfrc522_1.PCD_StopCrypto1();
+  if (servoActivo && now - servoInicio >= SERVO_MS) {
+    servo1.write(0);
+    servoActivo = false;
   }
 
-  if (mfrc522_2.PICC_IsNewCardPresent() && mfrc522_2.PICC_ReadCardSerial()) {
-    detectarTarjeta(mfrc522_2, 2, getUID(mfrc522_2));
-    mfrc522_2.PICC_HaltA();
-    mfrc522_2.PCD_StopCrypto1();
+  digitalWrite(SS_2_PIN, HIGH);
+  if (rfid1.PICC_IsNewCardPresent() && rfid1.PICC_ReadCardSerial()) {
+    String uid = uidToString(rfid1);
+    if (uid != lastUID1 || now - lastTime1 > DEBOUNCE_MS) {
+      lastUID1 = uid; lastTime1 = now;
+      mqtt.publish(TOPIC_RFID1, uid.c_str());
+      mqtt.publish(TOPIC_ACCESO1, "detectado");
+      abrirServo();
+    }
+    rfid1.PICC_HaltA(); rfid1.PCD_StopCrypto1();
   }
 
-  delay(200);
-}
-#include <DHT.h>
-#include <ESP32Servo.h>
-#define DHTPIN 25
-#define DHTTYPE DHT11
-int pin1 = 18;
-int pin2 = 19;
-int pin3 = 21;
-int pin4 = 22;
-int pin5 = 23;
-int rele = 17;
-Servo s1, s2, s3, s4, s5;
-DHT dht(DHTPIN, DHTTYPE);
-bool techoCerrado = false;
-int posicionActual = 0;
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  s1.attach(pin1);
-  s2.attach(pin2);
-  s3.attach(pin3);
-  s4.attach(pin4);
-  s5.attach(pin5);
-  pinMode(rele, OUTPUT);
-  digitalWrite(rele, LOW);
-}
-void moverServos(int destino) {
-  if (posicionActual < destino) {
-    for (int pos = posicionActual; pos <= destino; pos++) {
-      s1.write(pos);
-      s2.write(pos);
-      s3.write(pos);
-      s4.write(pos);
-      s5.write(pos);
-      delay(20);
+  digitalWrite(SS_1_PIN, HIGH);
+  if (rfid2.PICC_IsNewCardPresent() && rfid2.PICC_ReadCardSerial()) {
+    String uid = uidToString(rfid2);
+    if (uid != lastUID2 || now - lastTime2 > DEBOUNCE_MS) {
+      lastUID2 = uid; lastTime2 = now;
+      mqtt.publish(TOPIC_RFID2, uid.c_str());
+      mqtt.publish(TOPIC_ACCESO2, "detectado");
+      abrirServo();
     }
-  } else {
-    for (int pos = posicionActual; pos >= destino; pos--) {
-      s1.write(pos);
-      s2.write(pos);
-      s3.write(pos);
-      s4.write(pos);
-      s5.write(pos);
-      delay(20);
+    rfid2.PICC_HaltA(); rfid2.PCD_StopCrypto1();
+  }
+
+  int pir = digitalRead(PIR1_PIN);
+  if (pir == HIGH && pirAnterior == LOW) mqtt.publish(TOPIC_MOVIMIENTO1, "1");
+  pirAnterior = pir;
+
+  if (motoresActivos) {
+    if (!motoresEncendidos && now - ultimoEncendido >= INTERVALO_MOTOR) {
+      digitalWrite(L293D_IN1, HIGH);
+      digitalWrite(L293D_IN2, LOW);
+      analogWrite(L293D_ENA, VELOCIDAD);
+      motoresEncendidos = true;
+      ultimoEncendido   = now;
+    }
+    if (motoresEncendidos && now - ultimoEncendido >= DURACION_MOTOR) {
+      digitalWrite(L293D_IN1, LOW);
+      digitalWrite(L293D_IN2, LOW);
+      analogWrite(L293D_ENA, 0);
+      motoresEncendidos = false;
+      ultimoEncendido   = now;
     }
   }
-  posicionActual = destino;
-}
-void abrir_techo() {
-  moverServos(90);  
-}
-void cerrar_techo() {
-  moverServos(0);
-}
-void loop() {
-  float hum = dht.readHumidity();
-  float temp = dht.readTemperature();
-  if (isnan(hum) || isnan(temp)) {
-    Serial.println("Error leyendo el sensor");
-    delay(2000);
-    return;
-  }
-  Serial.print("Temperatura: ");
-  Serial.print(temp);
-  Serial.print(" °C  Humedad: ");
-  Serial.print(hum);
-  Serial.println(" %");
-  if (temp > 26 && !techoCerrado || hum>80) {
-    Serial.println("Cerrando techo");
-    cerrar_techo();
-    techoCerrado = true;
-    Serial.println("Activando riego");
-    digitalWrite(rele, HIGH);
-    delay(10000);
-    digitalWrite(rele, LOW);
-  } 
-  else if (temp <= 24 && hum<70 && techoCerrado) {
-    Serial.println("Abriendo techo");
-    abrir_techo();
-    techoCerrado = false;
-    Serial.println("Bomba apagada");
-    digitalWrite(rele, LOW);
-  }
-  if(temp>26 && hum<70){
-    Serial.println("Regando plantas");
-    digitalWrite(rele, HIGH);
-    delay(10000);
-    digitalWrite(rele,LOW);
-  }
-  delay(2000);
+
+  delay(50);
 }
